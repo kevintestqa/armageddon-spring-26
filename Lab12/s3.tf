325fbcac8d1a330e304adee95c83e57e8943b908
Kevin Willocks <kevinwillocks@gmail.com>
2026-07-30 14:32:08 -0500

    add versioning resource

--- Lab12/s3.tf
+++ Lab12/s3.tf
@@ -2,11 +2,14 @@ resource "aws_s3_bucket" "asgard_executive_report" {
   bucket        = "asgard-executive-report"
   force_destroy = true
 
-  versioning {
-    enabled = true
-  }
-
   tags = {
     Name = "Asgard Cloud Security Executive Report Bucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "asgard_executive_report_versioning" {
+  bucket = aws_s3_bucket.asgard_executive_report.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
