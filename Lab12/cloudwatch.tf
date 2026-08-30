3432cd79b5d11f4da577bc8ed592687092db073c
Kevin Willocks <kevinwillocks@gmail.com>
2026-08-30 12:25:05 -0500

    Add coment

--- Lab12/cloudwatch.tf
+++ Lab12/cloudwatch.tf
@@ -7,6 +7,7 @@ resource "aws_cloudwatch_log_group" "asgard_logs" {
   }
 }
 
+#TODO: CloudWatch metric extraction/display remains unresolved
 resource "aws_cloudwatch_log_resource_policy" "asgard_logs_resource_policy" {
   policy_document = data.aws_iam_policy_document.asgard_waf_log_policy.json
   policy_name     = "WAF-logging-policy"
